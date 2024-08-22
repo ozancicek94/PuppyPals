9cb6188ebe0fdb451b2438389feace8e568a2ee8
Ozan Cicek <ozancicek94@gmail.com>
2024-08-22 01:54:49 -0400

    css designed

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,36 @@ import {puppyList} from './data.js'
 function App() {
 
   const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
 
   console.log(puppies);
-  
+
+  const featPup = puppies.find(pup => featPupId === pup.id);
+  console.log(featPup)
 
   return (
     <div>
 
       {puppies.map(puppy => {
-        return <p>{puppy.name}</p>
-      })}
+        return <p className='puppyList' key={puppy.id} onClick={() => {setFeatPupId(puppy.id)}}>{puppy.name}</p>  
+      })
+      }
+
+      
+
+      { featPupId && ( <div>
+
+        <h2 className=''>{featPup.name}</h2>
+        <ul>
+          <li>Age: {featPup.age}</li>
+          <li>Email: {featPup.email}</li>
+        </ul>
+        
+         </div>)}
+
+      
+
+      
   
     </div>
   )
