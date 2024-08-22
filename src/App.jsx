6c77b1702af93b55d5b554455f5e0564cf51bea0
Ozan Cicek <ozancicek94@gmail.com>
2024-08-21 23:46:41 -0400

    puppy data added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {puppyList} from './data.js'
 
 function App() {
+
+  const [puppies, setPuppies] = useState(puppyList)
+
+  console.log(puppies);
   
 
   return (
     <div>
+
+      {puppies.map(puppy => {
+        return <p>{puppy.name}</p>
+      })}
   
     </div>
   )
